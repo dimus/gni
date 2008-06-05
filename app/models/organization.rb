520588569ae1aa69037cb16b0ff9c6ef69861a46
Dmitry Mozzherin <dmozzherin@gmail.com>
2008-06-05 17:54:10 -0400

    tests work

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,4 @@
 class Organization < ActiveRecord::Base
   has_many :participant_organizations
   has_many :organization_contacts
-  :class_name => "Person", :foreign_key => "person_id"
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,4 @@
 class Person < ActiveRecord::Base
-  has_many :organizations
   has_many :organization_contacts
   has_many :participant_contacts
   has_many :participant_people
