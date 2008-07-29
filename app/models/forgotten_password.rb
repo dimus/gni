57b6e14b93f3ff4e1248d78d20e6059ef5a67020
Dmitry Mozzherin <dmozzherin@gmail.com>
2008-07-29 00:43:18 -0400

    added mailer dirs

--- app/models/forgotten_password.rb
+++ app/models/forgotten_password.rb
@@ -1,4 +1,11 @@
 class ForgottenPassword < ActionMailer::Base
+
+  def new_password(user)
+    recipients user.email
+    subject "New password from GNA"
+    from "noreply@gnapartnership.org"
+    body :recipient => user.full_name
+  end
   
 
 end
