80c9392b9b5e88ef9c6ee84beef1f26ad2c878c5
Dmitry Mozzherin <dmozzherin@gmail.com>
2009-01-30 15:57:42 -0500

    work on api for name strings

--- app/controllers/name_strings_controller.rb
+++ app/controllers/name_strings_controller.rb
@@ -5,15 +5,24 @@ class NameStringsController < ApplicationController
   # GET /name_strings.xml
   def index
     page = params[:page] || 1
+    per_page = params[:per_page] || 50
     params[:name_string][:search_term] = params[:name_string][:search_term].strip.gsub(/\*/,'%') unless params[:name_string].nil? || params[:name_string][:search_term].nil?
     if params[:commit] == 'Search Mine'
-      @name_strings = NameString.paginate_by_sql(["select n.name from name_strings n join name_indices i on (n.id = i.name_string_id) join data_source_contributors c on (i.data_source_id = c.data_source_id)  where name like ? and c.user_id = ?", params[:name_string][:search_term], current_user.id], :page => page) || nil rescue nil
+      @name_strings = NameString.paginate_by_sql(["select n.name from name_strings n join name_indices i on (n.id = i.name_string_id) join data_source_contributors c on (i.data_source_id = c.data_source_id)  where name like ? and c.user_id = ?", params[:name_string][:search_term], current_user.id], :page => page, :per_page => per_page) || nil rescue nil
     else
-      @name_strings = NameString.paginate_by_sql(["select * from name_strings where name like ?", params[:name_string][:search_term]], :page => page) || nil rescue nil 
+      @name_strings = NameString.paginate_by_sql(["select * from name_strings where name like ?", params[:name_string][:search_term]], :page => page, :per_page => per_page) || nil rescue nil 
     end
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @name_strings }
+      format.xml  do
+        result = {}
+        result[:page_number] = page
+        result[:name_strings_total] = @name_strings.total_entries
+        result[:total_pages] = result[:name_strings_total]/per_page.to_i
+        result[:per_page] = per_page
+        result[:data] = @name_strings
+        render :xml => result
+      end
     end
   end
 
