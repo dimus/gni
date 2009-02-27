7eba2f3a3e966f7bf681e691e9735f63208edfe7
Dmitry Mozzherin <dmozzherin@gmail.com>
2009-02-27 14:08:04 -0500

    added api and help documentation placeholders

--- app/controllers/name_strings_controller.rb
+++ app/controllers/name_strings_controller.rb
@@ -5,7 +5,7 @@ class NameStringsController < ApplicationController
   # GET /name_strings.xml
   def index
     page = params[:page] || 1
-    per_page = 50 if params[:per_page].to_i < 1 
+    per_page = (params[:per_page].to_i < 1) ? 30 : params[:per_page].to_i 
     per_page = PER_PAGE_MAX if per_page > PER_PAGE_MAX
     search_term = params[:search_term].strip.gsub(/\*\s*$/,'%') rescue nil
     search_term = NameString.normalize_name_string(search_term) if search_term
@@ -16,15 +16,15 @@ class NameStringsController < ApplicationController
     if is_valid_search
       search_term = NameString.normalize_name_string(search_term)
       if params[:commit] == 'Search Mine'
-        @name_strings = NameString.paginate_by_sql(["select n.name from name_strings n join name_indices i on (n.id = i.name_string_id) join data_source_contributors c on (i.data_source_id = c.data_source_id)  where normalized_name like ? and c.user_id = ? order by n.normalized_name", search_term, current_user.id], :page => page, :per_page => per_page) || nil rescue nil
+        @name_strings = NameString.paginate_by_sql(["select n.id, n.name from name_strings n join name_indices i on (n.id = i.name_string_id) join data_source_contributors c on (i.data_source_id = c.data_source_id)  where normalized_name like ? and c.user_id = ? order by n.normalized_name", search_term, current_user.id], :page => page, :per_page => per_page) || nil rescue nil
       elsif params[:data_source_id]
         search_term ||= '%'
-        @name_strings = NameString.paginate_by_sql(["select n.* from name_strings n join name_indices i on (n.id = i.name_string_id) where normalized_name like ? and i.data_source_id = ? order by n.normalized_name", search_term, params[:data_source_id]], :page => page, :per_page => per_page) || nil rescue nil
+        @name_strings = NameString.paginate_by_sql(["select n.id, n.name from name_strings n join name_indices i on (n.id = i.name_string_id) where normalized_name like ? and i.data_source_id = ? order by n.normalized_name", search_term, params[:data_source_id]], :page => page, :per_page => per_page) || nil rescue nil
       else
-        @name_strings = NameString.paginate_by_sql(["select * from name_strings where normalized_name like ? order by normalized_name", search_term], :page => page, :per_page => per_page) || nil rescue nil
+        @name_strings = NameString.paginate_by_sql(["select id, name from name_strings where normalized_name like ? order by normalized_name", search_term], :page => page, :per_page => per_page) || nil rescue nil
       end
     else
-      @name_strings = NameString.paginate_by_sql("select * from name_strings where 1=2", :page => page, :per_page => per_page) || nil rescue nil 
+      @name_strings = NameString.paginate_by_sql("select 1 from name_strings where 1=2", :page => page, :per_page => per_page) || nil rescue nil 
       flash[:error]=search_term_errors
     end    
     
@@ -39,7 +39,7 @@ class NameStringsController < ApplicationController
     result[:total_pages] = (result[:name_strings_total]/(per_page.to_f)).ceil rescue 0
     result[:per_page] = per_page
     result[:next_page] = name_strings_url(:search_term => search_term, :per_page => per_page, :page => page.to_i + 1, :format=>:xml) unless result[:total_pages].to_i <= page.to_i
-    result[:data] = @name_strings
+    result[:name_strings] = @name_strings
     
     respond_to do |format|
         format.html # index.html.erb
