e3e5115b2a73d43bc43de116ee0837f2242f5401
Dmitry Mozzherin <dmozzherin@gmail.com>
2009-04-12 19:09:03 -0400

    work on parser gui

--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -1,15 +1,19 @@
 class ParsersController < ApplicationController
-  
-  # GET /parsers
-  def index
-    @names = params[:names]
-    @parsed_names = Parser.parse(@names)
-    render
-  end
 
   # GET /parsers/new
   def new
     render
+  end
+  
+  # POST /parsers
+  def create
+    @names = params[:file].read rescue params[:names] 
+    @parsed_names = Parser.parse(@names)
+    if params[:format] == 'json'
+      render :json => @parsed_names
+    else
+      render :xml => @parsed_names
+    end
   end
 
 end
