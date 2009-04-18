40deaf8b877350750dfde123f4d5df3fdcd1bc35
Dmitry Mozzherin <dmozzherin@gmail.com>
2009-04-17 22:43:40 -0400

    removed creation of new indices in name_index_records

--- db/migrate/20090417200241_data_sources_add_name_strings_count_column.rb
+++ db/migrate/20090417200241_data_sources_add_name_strings_count_column.rb
@@ -1,13 +1,9 @@
 class DataSourcesAddNameStringsCountColumn < ActiveRecord::Migration
   def self.up
     add_column :data_sources, :name_strings_count, :integer, :default => 0
-    add_index :name_index_records, [:kingdom_id], :name => :idx_name_index_records_3
-    add_index :name_index_records, [:global_id], :name => :idx_name_index_records_4
   end
 
   def self.down
     remove_column :data_sources, :name_strings_count
-    remove_index :name_index_records, :name => :idx_name_index_records_3
-    remove_index :name_index_records, :name => :idx_name_index_records_4
   end
 end
--- db/migrate/20090417202201_create_name_ranks.rb
+++ db/migrate/20090417202201_create_name_ranks.rb
@@ -7,7 +7,6 @@ class CreateNameRanks < ActiveRecord::Migration
     
     add_column :name_index_records, :name_rank_id, :integer
     execute("insert into name_ranks (select distinct null, rank, now(), now() from name_index_records where rank is not null) ")
-    add_index :name_index_records, :name_rank_id, :name => :idx_name_index_records_5
     execute("update name_index_records nir join name_ranks nr on nr.name = nir.rank set name_rank_id = nr.id")
     remove_column :name_index_records, :rank
   end
@@ -15,7 +14,6 @@ class CreateNameRanks < ActiveRecord::Migration
   def self.down
     add_column :name_index_records, :rank, :string
     execute("update name_index_records nir join name_ranks nr on nr.id = nir.name_rank_id set rank = nr.name")
-    remove_index :name_index_records, :name => :idx_name_index_records_5
     remove_column :name_index_records, :name_rank_id
     drop_table :name_ranks
   end
