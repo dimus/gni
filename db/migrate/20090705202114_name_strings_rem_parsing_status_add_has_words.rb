6df0dbc8b136c92c6d4fc8ac7775a0da8284e229
Dmitry Mozzherin <dmozzherin@gmail.com>
2009-07-06 00:05:09 -0400

    intermediate

--- db/migrate/20090705202114_name_strings_rem_parsing_status_add_has_words.rb
+++ db/migrate/20090705202114_name_strings_rem_parsing_status_add_has_words.rb
@@ -2,7 +2,7 @@ class NameStringsRemParsingStatusAddHasWords < ActiveRecord::Migration
   def self.up
     remove_column :name_strings, :parsing_status
     add_column :name_strings, :has_words, :boolean
-    add_index :name_strings, :has_words, :name => 'idx_name_strings_3'
+    #add_index :name_strings, :has_words, :name => 'idx_name_strings_3'
   end
 
   def self.down
--- lib/gni_search.rb
+++ lib/gni_search.rb
@@ -3,6 +3,7 @@ module GNI
     def initialize(transaction_limit = 200000)
       @c = ActiveRecord::Base.connection
       @names = get_names
+      @names_order = nil
       @semantics = get_semantics
       @transaction_limit = transaction_limit
       @parser = nil
@@ -19,9 +20,15 @@ module GNI
     def generate_words
       start_transaction
       count = 0
-      @names.each do |name|
+      @names.each do |name_array|
         count += 1
+        
         if count % @transaction_limit == 0
+          name_string_id, name, canonical_form_id = name_array
+          words = NameWord.split_name(name)
+          positions = @parser.parse(name).pos
+          generate_words(words, positions, name_string_id)
+          insert_canonical_form(name_string_id, name) unless canonical_form_id
           end_transaction
           start_transaction
         end
@@ -31,7 +38,7 @@ module GNI
       
   protected
     def get_names
-      @c.execute('select ns.id, ns.name, ns.canonical_form_id, cf.name from name_strings ns left join canonical_forms cf on cf.id = ns.canonical_form_id where ns.has_words = 0 or ns.has_words is null')
+      @c.execute('select ns.id, ns.name, ns.canonical_form_id where ns.has_words = 0 or ns.has_words is null')
     end
     
     def get_semantics
@@ -46,6 +53,36 @@ module GNI
     def end_transaction
       @c.execute('commit') if RAILS_ENV != 'test'
     end
-  end
+    
+    def generate_words(words, positions, name_string_id)
+      words.each do |word|
+        semantic_meaning = (positions[word[0]] ? positions[word[0]][0] : nil) rescue nil
+        word << semantic_meaning
+        sem_id = @semantics[word[2].to_sym] ? @semantics[word[2].to_sym] : nil rescue nil
+        name_word_id = @c.select_value("select id from name_words where word = '%s'" % word[1])
+        unless nw
+          name_word_id = insert_name_word(word)
+        end
+        insert_name_word_semantics(word,name_word_id, name_string_id)
+      end
+    end
+    
+    def insert_name_word(word_array)
+      #this will only be correct with ruby 1.9
+      word_size = word_array[1].size
+      first_letter = @c.gni_sanitize_sql(word[1][0])
+      word[1] = @c.gni_sanitize_sql(word[1])
+      q = "insert into name_words (word, first_letter, length, created_at, updated_at) values ('%s', '%s', %s, now(), now())" % [word[1], first_letter, word_size]
+      dbh.query(q)
+      @c.select_value("select last_insert_id()")
+    end
   
+    def insert_name_word_semantics(word_array, name_word_id, name_string_id)
+      semantic_value = word_array[2].to_sym rescue ''
+      semantic_meaning_id = @semantics[semantic_value] ? @semantics[semantic_value] : 'null'
+      q = "insert into name_word_semantics (name_word_id, name_string_id, semantic_meaning_id, name_string_position, created_at, updated_at) values (%s, %s, %s, %s, now(), now())" % [name_word_id, name_string_id, semantic_meaning_id, word_array[0]]
+      @c.execute(q)      
+    end
+    
+  end
 end
