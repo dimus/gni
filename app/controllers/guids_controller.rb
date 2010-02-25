3d90b4e3781587e25f6b18703cb4a4666df89e55
Dmitry Mozzherin <dmozzherin@gmail.com>
2010-02-25 14:25:32 -0500

    small fixes

--- app/controllers/guids_controller.rb
+++ app/controllers/guids_controller.rb
@@ -2,17 +2,16 @@ class GuidsController < ApplicationController
   # GET /guids
   def index
     if params[:names]
-    @names = params[:names].gsub(/[;|]/, "\n")
-    @guids = {:uuid_type => 5, :namespace => $GNA_NAMESPACE}
-    guids_hash
-    @names.each do |name|
-      UUID.create_v5(NameString.normalize(name)).guid
-    end
-  
-    respond_to do |format|
-      format.html
-      format.xml 
-      format.json
+      @names = params[:names].gsub(/[;|]/, "\n")
+      @guids = {:uuid_type => 5, :namespace => $GNA_NAMESPACE}
+      @names.each do |name|
+        UUID.create_v5(NameString.normalize(name)).guid
+      end
+      respond_to do |format|
+        format.html
+        format.xml 
+        format.json
+      end
     end
   end
 end
