1c3e1942bd5ca482090eeb5cc28bc59f696b9e6f
Dmitry Mozzherin <dmozzherin@gmail.com>
2010-05-13 20:02:13 -0400

    removed a slow query from unique names

--- app/models/unique_name.rb
+++ app/models/unique_name.rb
@@ -4,8 +4,12 @@ class UniqueName < ActiveRecord::Base
   
   def self.update_unique_names
     ActiveRecord::Base.connection.transaction do
-      ActiveRecord::Base.connection.execute("delete from unique_names");
-      ActiveRecord::Base.connection.execute("insert into unique_names (select null, data_source_id, name_string_id, now(), now() from name_indices group by name_string_id having count(*) = 1)");    
+      ActiveRecord::Base.connection.execute("drop table if exists tmp_unique_names");
+      ActiveRecord::Base.connection.execute("create table tmp_unique_names like unique_names");
+      ActiveRecord::Base.connection.execute("insert into tmp_unique_names (select null, data_source_id, name_string_id, now(), now() from name_indices group by name_string_id having count(*) = 1)");    
+      ActiveRecord::Base.connection.execute("rename table tmp_unique_names to unique_names");
+      ActiveRecord::Base.connection.execute("rename table unique_names to old_unique_names");
+      ActiveRecord::Base.connection.execute("drop table if exists old_unique_names");
     end
   end
   
